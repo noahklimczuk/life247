92b33137aa97241e748edcf0ea2c2658e2384d16
Noah Klimczuk <noahklimczuk2001@gmail.com>
2026-06-08 20:05:07 -0400

    Initial Commit

---
+++ life247/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  life247
+//
+//  Created by Noah Klimczuk on 2026-06-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ life247/life247App.swift
@@ -0,0 +1,17 @@
+//
+//  life247App.swift
+//  life247
+//
+//  Created by Noah Klimczuk on 2026-06-08.
+//
+
+import SwiftUI
+
+@main
+struct life247App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
